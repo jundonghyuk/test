e888988335c230ccd46292b2b383ba885677df22
JunDonghyuk <ehdgur4814@naver.com>
2023-08-07 11:29:30 +0900

    test:

--- database/src/main/java/com/test/database/api/MemberRepository.java
+++ database/src/main/java/com/test/database/api/MemberRepository.java
@@ -1,6 +1,13 @@
 package com.test.database.api;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Member> findById(Long id);
 }
